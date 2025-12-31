b182590967746415eb84329da2549de1fa09a3ef
Oleksandr Maliuk <maliukolexandr@gmail.com>
2026-01-01 00:06:37 +0200

    feat: enhance AboutMeSection with translations, image support, and improved content structure

--- modules/components/01_HomePage/02_AboutMe/AboutMe.tsx
+++ modules/components/01_HomePage/02_AboutMe/AboutMe.tsx
@@ -1,18 +1,42 @@
-import clsx from "clsx"
-import css from './AboutMe.module.css';
+import clsx from "clsx";
+import css from "./AboutMe.module.css";
+import { useTranslations } from "next-intl";
+import Image from "next/image";
 
 export default function AboutMeSection() {
+  const t = useTranslations("AboutMe");
   return (
     <section id="about" className="section">
       <div className={clsx("container", css.aboutMeContainer)}>
-        <h2 className="text-3xl font-bold mb-6">About Me</h2>
-        <p className="text-lg leading-relaxed">
-          Hello! I&apos;m Stasya, a certified fitness trainer and nutrition
-          enthusiast. My mission is to help you achieve your health and fitness
-          goals through personalized training programs and nutritional guidance.
-          With years of experience in the industry, I am dedicated to empowering
-          individuals to lead healthier, happier lives.
-        </p>
+        <h2 className="text-3xl">{t("title")}</h2>
+        <p className="text">{t("description.part1")}</p>
+        <picture>
+          <source
+            srcSet="/images/about-me.jpg"
+            type="image/webp"
+            width={600}
+            height={400}
+            media="(min-width: 1440px)"
+            className={css.aboutMeImage}
+          />
+          <source
+            srcSet="/images/about-me.jpg"
+            type="image/jpeg"
+            width={600}
+            height={400}
+            media="(max-width: 1439px)"
+            className={css.aboutMeImage}
+          />
+          <Image
+            src="/images/about-me.jpg"
+            alt={"About Me Image"}
+            width={800}
+            height={600}
+            loading="lazy"
+            className={css.aboutMeImage}
+          />
+        </picture>
+        <p className="text">{t("description.part2")}</p>
       </div>
     </section>
   );
