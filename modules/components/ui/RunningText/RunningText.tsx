3aece900ea10d0542b64907022f36f9c40a7a3e2
Oleksandr Maliuk <maliukolexandr@gmail.com>
2025-12-31 18:28:28 +0200

    feat: adjust RunningText animation duration and improve markup structure

--- modules/components/ui/RunningText/RunningText.tsx
+++ modules/components/ui/RunningText/RunningText.tsx
@@ -4,7 +4,12 @@ export default function RunningText() {
   return (
     <div className={css.marquee}>
       <div className={css.track}>
-        {Array.from({ length: 30 }).map((_, i) => (
+        {Array.from({ length: 30}).map((_, i) => (
+          <span key={i}>Training</span>
+        ))}
+      </div>
+      <div className={css.track}>
+        {Array.from({ length: 30}).map((_, i) => (
           <span key={i}>Training</span>
         ))}
       </div>
