07f88fed80644b17762359211c6e0df89d9cf3c4
Oleksandr Maliuk <maliukolexandr@gmail.com>
2026-01-06 03:58:23 +0200

    fix: remove unnecessary whitespace in AboutMeSection component and update linkButton styles

--- modules/components/01_HomePage/02_AboutMe/AboutMe.tsx
+++ modules/components/01_HomePage/02_AboutMe/AboutMe.tsx
@@ -15,7 +15,6 @@ export default function AboutMeSection() {
         <div className={css.content1}>
           <h2 className={css.title}>{t("intro.title")}</h2>
           <p className={css.text}>{t("intro.texts.0")}</p>
-
           <Image
             src="/AboutMe1.webp"
             alt={"About Me Image"}
@@ -29,7 +28,6 @@ export default function AboutMeSection() {
 
         <div className={css.content2}>
           <h3 className={css.title}>{ht("title")}</h3>
-
           <Image
             src="/AboutMe2.webp"
             alt={"About Me Image"}
@@ -45,7 +43,6 @@ export default function AboutMeSection() {
 
         <div className={css.content3}>
           <h3 className={css.title}>{t("story.title")}</h3>
-
           <div className={css.story}>
             <Image
               src="/AboutMe3.webp"
@@ -55,7 +52,6 @@ export default function AboutMeSection() {
               loading="lazy"
               className={css.image3}
             />
-
             <ul className={css.storyList}>
               {paragraphs.map((p, i) => (
                 <li className={css.listItem} key={i}>{p}</li>
