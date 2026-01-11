0331170be27ed718be566131ff1b40632ba87ff8
Oleksandr Maliuk <maliukolexandr@gmail.com>
2026-01-11 18:12:17 +0200

    feat: reduce animation duration and limit text repetition in RunningText component

--- modules/components/@ui/RunningText/RunningText.tsx
+++ modules/components/@ui/RunningText/RunningText.tsx
@@ -6,16 +6,15 @@ import css from './RunningText.module.css';
 export default function RunningText() {
   const t = useTranslations('runingText');
   const texts = t.raw('texts') as string[];
-  console.log(texts);
   return (
     <div className={css.marquee}>
       <div className={css.track}>
-        {Array.from({ length: 30 }).map((_, i) =>
+        {Array.from({ length: 5 }).map((_, i) =>
           texts.map((text, textIndex) => <span key={`${i}-${textIndex}`}>{text}</span>)
         )}
       </div>
       <div className={css.track}>
-        {Array.from({ length: 30 }).map((_, i) =>
+        {Array.from({ length: 5 }).map((_, i) =>
           texts.map((text, textIndex) => <span key={`${i}-${textIndex}`}>{text}</span>)
         )}
       </div>
